2e9f2a8e08f0937aee0c07902252a7a80af4afd2
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-10-03 21:28:46 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems
+  .map((image) => {
+    return `
+        <a class = "gallery__item" href= "${image.original}">
+            <img class = "gallery__image" 
+                src = ${image.preview} 
+                alt = "${image.description}">
+         </a>`;
+  }).join('');
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+    
