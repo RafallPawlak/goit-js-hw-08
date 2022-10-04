59165a4b231037046aa1a3e46507f62ff5aa6baa
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-10-04 21:21:18 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,7 @@ import Player from '@vimeo/player';
 const _ = require('lodash');
 
 const player = new Player('vimeo-player');
-//     ,{
-//     id: 19231868,
-//     width: 640
-// });
-    
+
 player.on('timeupdate', _.throttle(() => {
     player.getCurrentTime().then(function (seconds) {
         localStorage.setItem("videoplayer-current-time", seconds);
@@ -14,6 +10,7 @@ player.on('timeupdate', _.throttle(() => {
 }, 1000));
 
 window.addEventListener('load', pageOpened, false);
+
 function pageOpened() {
     const lastPlayTime = localStorage.getItem("videoplayer-current-time");
     player.setCurrentTime(lastPlayTime);
